c1f181f43df8246a2a1ee53ac7e3266408a52a3e
aid219 <aid219@mail.ru>
2024-11-18 13:46:25 +0300

    gsdfgsdf

--- __init__.py
+++ __init__.py
@@ -1,8 +1,92 @@
-DOMAIN = "hello_state"
+"""The LED BLE integration."""
+
+from __future__ import annotations
+
+import asyncio
+from datetime import timedelta
+import logging
 
 
-async def async_setup(hass, config):
-    hass.states.async_set("hello_state.world", "Paulus")
+from homeassistant.components import bluetooth
+from homeassistant.components.bluetooth.match import ADDRESS, BluetoothCallbackMatcher
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import CONF_ADDRESS, EVENT_HOMEASSISTANT_STOP, Platform
+from homeassistant.core import Event, HomeAssistant, callback
+from homeassistant.exceptions import ConfigEntryNotReady
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
 
-    # Return boolean to indicate that initialization was successful.
-    return True
+from .const import DEVICE_TIMEOUT, DOMAIN, UPDATE_SECONDS
+
+
+PLATFORMS: list[Platform] = [Platform.LIGHT]
+
+_LOGGER = logging.getLogger(__name__)
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up LED BLE from a config entry."""
+    address: str = entry.data[CONF_ADDRESS]
+    ble_device = bluetooth.async_ble_device_from_address(hass, address.upper(), True)
+    if not ble_device:
+        raise ConfigEntryNotReady(
+            f"Could not find LED BLE device with address {address}"
+        )
+
+    @callback
+    def _async_update_ble(
+        service_info: bluetooth.BluetoothServiceInfoBleak,
+        change: bluetooth.BluetoothChange,
+    ) -> None:
+        """Update from a ble callback."""
+
+    entry.async_on_unload(
+        bluetooth.async_register_callback(
+            hass,
+            _async_update_ble,
+            BluetoothCallbackMatcher({ADDRESS: address}),
+            bluetooth.BluetoothScanningMode.PASSIVE,
+        )
+    )
+
+    async def _async_update() -> None:
+        """Update the device state."""
+
+    startup_event = asyncio.Event()
+
+    coordinator = DataUpdateCoordinator(
+        hass,
+        _LOGGER,
+        config_entry=entry,
+        update_method=_async_update,
+        update_interval=timedelta(seconds=UPDATE_SECONDS),
+    )
+
+    try:
+        await coordinator.async_config_entry_first_refresh()
+    except ConfigEntryNotReady:
+
+        raise
+
+    try:
+        async with asyncio.timeout(DEVICE_TIMEOUT):
+            await startup_event.wait()
+    except TimeoutError as ex:
+        raise ConfigEntryNotReady("Unable to communicate with the device; ") from ex
+
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    entry.async_on_unload(entry.add_update_listener(_async_update_listener))
+
+
+async def _async_update_listener(hass: HomeAssistant, entry: ConfigEntry) -> None:
+    """Handle options update."""
+    if entry.title != 0:
+        await hass.config_entries.async_reload(entry.entry_id)
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
+        data: 0
+        await data.device.stop()
+
+    return unload_ok
