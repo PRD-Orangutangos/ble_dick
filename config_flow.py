4f7b68cb4839765508c0c86a2eee941ae80b0f0b
aid219 <aid219@mail.ru>
2024-11-18 11:58:00 +0300

    33

--- config_flow.py
+++ config_flow.py
@@ -1,11 +1,10 @@
 from homeassistant import config_entries
 from homeassistant.core import callback
-from homeassistant.helpers import config_validation as cv
 import voluptuous as vol
 from .const import DOMAIN
 
 
-class MyBLEDeviceConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+class MyBLEDeviceConfigFlow(config_entries.ConfigFlow):
     """Handle a config flow for My BLE Device."""
 
     VERSION = 1
@@ -15,11 +14,18 @@ class MyBLEDeviceConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         if user_input is not None:
             return self.async_create_entry(title="My BLE Device", data=user_input)
 
-        return self.async_show_form(step_id="user")
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema(
+                {
+                    vol.Required("mac_address"): str,
+                }
+            ),
+        )
 
     @callback
-    async def async_step_ble(self, discovery_info):
-        """Обработать автоматически найденное устройство."""
+    async def async_step_discovery(self, discovery_info):
+        """Обработать автоматически найденное устройство через Bluetooth."""
         mac_address = discovery_info["mac"]
         name = discovery_info.get("name", "Unknown Device")
         return self.async_create_entry(title=name, data={"mac_address": mac_address})
