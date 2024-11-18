d0a97308963e8be6a91ddf4cd4bbd41f929fff2c
aid219 <aid219@mail.ru>
2024-11-18 15:18:47 +0300

    fsdf

--- __init__.py
+++ __init__.py
@@ -1,18 +1,29 @@
-"""Example Load Platform integration."""
+"""
+The "hello world" custom component.
+
+This component implements the bare minimum that a component should implement.
+
+Configuration:
+
+To use the hello_world component you will need to add the following to your
+configuration.yaml file.
+
+hello_world:
+"""
 
 from __future__ import annotations
 
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.typing import ConfigType
 
-DOMAIN = "example_load_platform"
+# The domain of your component. Should be equal to the name of your component.
+DOMAIN = "hello_world"
 
 
 def setup(hass: HomeAssistant, config: ConfigType) -> bool:
-    """Your controller/hub specific code."""
-    # Data that you want to share with your platforms
-    hass.data[DOMAIN] = {"temperature": 23}
+    """Set up a skeleton component."""
+    # States are in the format DOMAIN.OBJECT_ID.
+    hass.states.set("hello_world.Hello_World", "Works!")
 
-    hass.helpers.discovery.load_platform("sensor", DOMAIN, {}, config)
-
+    # Return boolean to indicate that initialization was successfully.
     return True
