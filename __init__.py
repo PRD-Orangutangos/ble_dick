5561f26e47f456ca41ee59a416fe4cad56d07086
aid219 <aid219@mail.ru>
2024-11-18 16:03:33 +0300

    adadada

--- __init__.py
+++ __init__.py
@@ -8,7 +8,7 @@ from . import hub
 
 # List of platforms to support. There should be a matching .py file for each,
 # eg <cover.py> and <sensor.py>
-PLATFORMS = [Platform.SENSOR, Platform.COVER]
+PLATFORMS = [Platform.SENSOR]
 
 type HubConfigEntry = ConfigEntry[hub.Hub]
 
